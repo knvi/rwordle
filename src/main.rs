2c186c7dca239b62e4217f362fe96c4650412abd
clima <rotherexram@gmail.com>
2022-04-23 10:28:57 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,135 @@
+use colored::*;
+<<<<<<< HEAD
+use rand::seq::SliceRandom;
+use std::{
+    collections::HashMap,
+};
+
+const ALL_WORDS: &str = include_str!("words.txt");
+
+// returns a vector of all the words in the file
+fn words_list() -> Vec<&'static str> {
+    ALL_WORDS
+        .split('\n')
+        .collect()
+}
+
+// returns a random item from a string vector
+fn random_word(words: &Vec<&'static str>) -> String {
+    let mut rng = rand::thread_rng();
+    words.choose(&mut rng).unwrap().to_string()
+}
+
+// get user's input and then check it in a viable words hashmap 
+fn get_user_input(viable_words: &HashMap<&'static str, i32>) -> String {
+    loop {
+        let mut input: String = String::new();
+        std::io::stdin().read_line(&mut input)
+            .expect("Error reading input");
+        let input = input.trim().to_lowercase();
+        if viable_words.contains_key(&input as &str) {
+            return input;
+        } else {
+            println!("That's not a valid word. Please try again!");
+=======
+use rand::prelude::SliceRandom;
+
+const ALL_WORDS: &str = include_str!("words.txt");
+fn sanitize_word(word: &str) -> String {
+    word.trim()
+        .to_uppercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .collect()
+}
+
+// returns a vector of all the words in the file
+fn words_list() -> Vec<String> {
+    ALL_WORDS
+        .split('\n')
+        .map(sanitize_word)
+        .collect()
+}
+
+struct Game {
+    dictionary: Vec<String>,
+    word: String,
+    guesses: Vec<String>,
+}
+
+impl Game {
+    fn new() -> Self {
+        let dictionary = words_list();
+        let word = dictionary.choose(&mut rand::thread_rng()).unwrap().to_string();
+        let guesses = vec![];
+        Game {
+            dictionary,
+            word,
+            guesses,
+>>>>>>> 7225ea09c47afee4b85b5e774608e99aeb104b3e
+        }
+    }
+}
+
+<<<<<<< HEAD
+fn display_format(word: String, actual_word: String) {
+    word.chars().enumerate().for_each(|(pos, c)| {
+        let display = if actual_word.chars().nth(pos).unwrap() == c { 
+            format!("{c}").to_uppercase().bright_green()
+        } else if actual_word.chars().any(|wc| wc == c) {
+            format!("{c}").to_uppercase().bright_yellow()
+        } else {
+            format!("{c}").to_uppercase().red()
+        };
+        print!("{display}");
+    });
+
+    println!();
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let words = words_list(); 
+    let hash: HashMap<&'static str, i32> = words.clone()
+                                    .into_iter()
+                                    .map(|x| (x,1))
+                                    .collect();
+    const MAX_GUESSES: u32 = 6;
+
+    loop {
+        let answer = random_word(&words);
+
+        let mut win: bool = false;
+
+        for _ in 0..MAX_GUESSES {
+            println!("Guess a word!");
+
+            let guess : String = get_user_input(&hash);
+
+            display_format(guess.clone(), answer.clone());
+
+            if guess == answer {
+                win = true;
+                break;
+            }
+        }
+
+        if win {
+            println!("You win!");
+        } else {
+            println!("You didn't win! The correct word was {}.", answer.to_uppercase());
+        }
+
+        println!("\nPlay again? (Y/n)");
+        let mut again : String = String::new();
+        std::io::stdin().read_line(&mut again)
+                   .expect("Failed to read line");
+        if again.trim().to_lowercase().chars().next().unwrap() == 'y' { continue; } 
+        else { break; }
+    }
+=======
+fn main() {
+    println!("Hello, world!");
+>>>>>>> 7225ea09c47afee4b85b5e774608e99aeb104b3e
+}
